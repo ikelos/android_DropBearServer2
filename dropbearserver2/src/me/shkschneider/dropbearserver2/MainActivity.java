bd78fea83c97ece78499efe0fade7a7c2f615c30
Alan SCHNEIDER <alan.schneider@cy-play.com>
2012-11-23 16:02:49 +0100

    Basic Android Project

---
+++ dropbearserver2/src/me/shkschneider/dropbearserver2/MainActivity.java
@@ -0,0 +1,20 @@
+package me.shkschneider.dropbearserver2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
